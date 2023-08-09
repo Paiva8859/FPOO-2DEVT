e85d8037937d6505cd93731757e4a63164b562df
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2023-08-09 16:48:38 -0300

    09/08/23

---
+++ Colecoes/App.java
@@ -0,0 +1,18 @@
+package Colecoes;
+
+/**
+ * App
+ */
+public class App {
+    public static void main(String[] args) {
+        ExemploArrayList obj1 = new ExemploArrayList();
+        ExemploLinkedList obj2 = new ExemploLinkedList();
+        ExemploHashMap obj3 = new ExemploHashMap();
+
+        obj1.test();
+        obj1.exercicio1();
+        obj2.test();
+        obj3.test();
+        obj3.exercicio1(); 
+    }
+}
