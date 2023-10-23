304fdf8840851e21c7f6beba7f0bed87fb165995
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2023-10-23 16:56:28 -0300

    a

---
+++ Serializacao/src/Usuario.java
@@ -0,0 +1,16 @@
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+private String nome;
+private int idade;
+public Usuario(String nome, int idade) {
+this.nome = nome;
+this.idade = idade;
+}
+public String getNome() {
+return nome;
+}
+public int getIdade() {
+return idade;
+}
+}
